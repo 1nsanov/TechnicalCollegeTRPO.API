8c771c62d1f92825e12d11599ce4a1ec1d85fa56
Виктор Безунов <victorbezunov@gmail.com>
2022-12-04 22:49:51 +0200

    init

---
+++ TechnicalCollegeTRPO.API/Controllers/ControllerBase.cs
@@ -0,0 +1,14 @@
+﻿using AspTestStage.Database;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspTestStage.BaseClasses;
+
+public class ControllerBase : Controller
+{
+    internal readonly AppDbContext _db;
+
+    public ControllerBase(AppDbContext db)
+    {
+        _db = db;
+    }
+}
--- TechnicalCollegeTRPO.API/Program.cs
+++ TechnicalCollegeTRPO.API/Program.cs
@@ -1,6 +1,46 @@
+using System.Text;
+using AspTestStage.Database;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers()
+    .AddJsonOptions(opts => opts.JsonSerializerOptions.PropertyNamingPolicy = null);
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(option =>
+    {
+        option.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            ClockSkew = TimeSpan.Zero,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                builder.Configuration["Jwt:Key"]))
+        };
+    });
+builder.Services.AddMvc();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+));
+
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.MapGet("/", () => "Run is success!");
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true)
+    .AllowCredentials());
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
